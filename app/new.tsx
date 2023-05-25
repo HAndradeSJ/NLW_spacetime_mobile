6325d71fa15f29a3703d6cb00bdc8f1292f82f74
HAndradeSJ <henriqueandradesj@outlook.com>
2023-05-24 22:56:54 -0300

    version mobile

--- app/new.tsx
+++ app/new.tsx
@@ -1,14 +1,30 @@
-import { Text, View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import NLWLogo from '../src/assets/logo.svg'
+import Icons from '@expo/vector-icons/Feather'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { Link } from 'expo-router'
 
 export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+  const [ispublic, setispublic] = usestate(false)
   return (
-    <View className="flex-1 px-8">
-      <View className='flex-row'>
+    <View
+      className="flex-1 px-8"
+      style={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="flex-row  mt-4 items-center justify-between">
         <NLWLogo />
 
-        <Link href="/memories" className='h-10 w-10 items-center justify-center rounded-full bg-purple-500'></Link>
+        <Link href="/memories" asChild>
+          <TouchableOpacityy className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icons name="arrow-left" size={16} color={#fff} />
+          </TouchableOpacity>
+        </Link>
+      </View>
+      <View className="mt-6 spcae-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch value={ispublic} onValueChange={setispublic} thumbColor={'#5c3ea3'}/>
+        </View>
       </View>
     </View>
   )
